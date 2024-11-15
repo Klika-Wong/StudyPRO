623fef348dcd78b3c9ea821cdad6f925e6e2736e
Klika-Wong <klikawong@163.com>
2024-11-15 14:04:56 +0800

    Update nav_app.py

--- StudyPRO/nav_app.py
+++ StudyPRO/nav_app.py
@@ -1,26 +1,7 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
 
 
-
-# 加载 .env 文件
-load_dotenv()
-
-# 获取环境变量
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_database = os.getenv("DB_DATABASE")
-
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL database!");
-});
-
-
+st.image("./logo.png", width=100)
 st.title("enter id")
 user_id = st.text_input("user ID:")
 if user_id:
